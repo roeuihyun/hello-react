342e19bd0b1fdcfe88acecd84838b7dfb33d84e9
roeuihyun <roeuihyun@gmail.com>
2021-07-17 12:38:53 +0900

    Using JSX

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // const title = React.createElement('h1',null,'JSX Test');
 // const text = React.createElement('p',null,'Paragraph Text~');
-// const container = React.createElement('p',null,[title, text]);
+// const container = React.createElement('div',null,[title, text]);
 
 ReactDOM.render(
     <div>
-        <h1>JSX Test</h1>
+        <h1>JSX Test{}</h1>
         <p>Paragraph Text~</p>
     </div>,
-  document.getElementById('root ')
+    document.getElementById('root ')
 );
 
 // If you want to start measuring performance in your app, pass a function
